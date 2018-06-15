a7e2222be30a3c06f84ac9881c89a995f6f6268d
Alexey Chernov <alexey.chernov@veeam.com>
2018-06-16 00:09:45 +0300

    Add CLI options

--- TFSAddLink/Program.cs
+++ TFSAddLink/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Net;
 
 namespace TFSAddLink
 {
     class Program
     {
+        class Options
+        {
+            [Option('t', "tfs", Required = true, HelpText = "TFS Server address")]
+            public string TFSAddress { get; set; }
+
+            [Option('u', "username", Required = true, HelpText = "Username")]
+            public string Username { get; set; }
+
+            [Option('p', "password", Required = true, HelpText = "Password")]
+            public string Password { get; set; }
+
+            [Option('i', "item", Required = true, HelpText = "Work item id")]
+            public string WorkItemID { get; set; }
+
+            [Option('l', "link", Required = true, HelpText = "Link to add")]
+            public string Link { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            var result = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options => AddLink(options));
+        }
+        static void AddLink(Options options)
+        {
+            Console.WriteLine(options.Username);
         }
     }
 }
